90b911567ecaa2a8daa006807a25f9a19d957521
LuZhangBo <62671678+LuZhangBo@users.noreply.github.com>
2020-05-15 22:26:17 +0300

    Add files via upload

---
+++ timing.py
@@ -0,0 +1,33 @@
+import time
+
+
+class Timing:
+    def __init__(self, function_to_run):
+        self.num_runs = 10
+        self.function_to_run = function_to_run
+
+    def __call__(self, *args, **kwargs):
+        avg = 0
+        for _ in range(self.num_runs):
+            t0 = time.time()
+            self.function_to_run(*args, **kwargs)
+            t1 = time.time()
+            avg += (t1 - t0)
+        avg /= self.num_runs
+        fn = self.function_to_run.__name__
+        print(
+            "[Timing] Среднее время выполнения %s за %s запусков: %.5f секунд" % (
+                fn,
+                self.num_runs,
+                avg
+            )
+        )
+        return self.function_to_run(*args, **kwargs)
+
+# код может быть вызван следующим декоратором:
+
+
+@Timing
+def foo(some_param):
+    for _ in range(some_param):
+        pass
